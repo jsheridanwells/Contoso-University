577b11f6ce879a6a47b8f2968a69b69db18e1168
Jeremy <jsheridanwells@gmail.com>
2018-02-17 10:16:18 -0600

    student details controller method and view

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,20 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Contoso.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Contoso.Data;
+using Contoso.Models;
 
 namespace Contoso.Controllers
 {
     public class StudentsController : Controller
     {
-        private readonly SchoolContext _context;
-    public StudentsController(SchoolContext context) => _context = context;
+      private readonly SchoolContext _context;
+      public StudentsController(SchoolContext context) => _context = context;
 
-    // GET: Students
-    public async Task<IActionResult> Index()
-    {
-      return View(await _context.Students.ToListAsync());
-    }
+      // GET: Students
+      public async Task<IActionResult> Index()
+      {
+        return View(await _context.Students.ToListAsync());
+      }
 
+      // GET: Students/Details/5
+      public async Task<IActionResult> Details(int? id)
+      {
+        if (id == null) return NotFound();
+
+        var student = await _context.Students
+          .Include(s => s.Enrollments)
+            .ThenInclude(e => e.Course)
+          .AsNoTracking()
+          .SingleOrDefaultAsync(m => m.Id == id);
+        
+        if (student == null) return NotFound();
+
+        return View(student);
+      }
   }
 }
