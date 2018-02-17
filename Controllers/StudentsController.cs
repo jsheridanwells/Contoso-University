0f45961c204817a103709fdae45bf392afb73aff
Jeremy <jsheridanwells@gmail.com>
2018-02-17 09:26:28 -0600

    students controller and views created

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,15 +1,20 @@
+using System.Threading.Tasks;
 using Contoso.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contoso.Controllers
 {
     public class StudentsController : Controller
     {
         private readonly SchoolContext _context;
-        public StudentsController(SchoolContext context)
-        {
-            _context = context;
-            
-        }
+    public StudentsController(SchoolContext context) => _context = context;
+
+    // GET: Students
+    public async Task<IActionResult> Index()
+    {
+      return View(await _context.Students.ToListAsync());
     }
+
+  }
 }
