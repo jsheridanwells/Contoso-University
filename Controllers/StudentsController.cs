fb564be86960dae164d860d80ce4a165a52d8d0a
Jeremy <jsheridanwells@gmail.com>
2018-02-17 10:50:26 -0600

    edit method and view working for students

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -61,9 +61,56 @@ namespace Contoso.Controllers
         }
         catch (DbUpdateException /* ex */)
         {
-          ModelState.AddModelError("", "Problem creating student.");
+          ModelState.AddModelError("", "No pudo crear nuevo alumno. Trate de nuevo.");
         }
         return View(student);
       }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.SingleOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+          if (id == null) return NotFound();
+
+          var studentToUpdate = await _context.Students.SingleOrDefaultAsync(s => s.Id == id);
+
+          if (await TryUpdateModelAsync<Student>(
+            studentToUpdate,
+            "",
+            s => s.FirstMidName, 
+            s => s.LastName,
+            s => s.EnrollmentDate
+          ))
+          {
+            try
+            {
+              await _context.SaveChangesAsync();
+              return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+              ModelState.AddModelError("", "No pudo actualizar nuevo alumno. Trate de nuevo.");
+            }
+          }
+
+          return View(studentToUpdate);
+        }
   }
 }
