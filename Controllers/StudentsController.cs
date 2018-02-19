2d0c941c546f7014902e523e44d95df6195b0e01
Jeremy <jsheridanwells@gmail.com>
2018-02-19 10:23:16 -0600

    student #index filter is working

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -18,12 +18,21 @@ namespace Contoso.Controllers
 
       // GET: Students
       // accesses Students/index.html
-      public async Task<IActionResult> Index(string sortOrder)
+      public async Task<IActionResult> Index(string sortOrder, string searchString)
       {
         ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
         ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+        ViewData["CurrentFilter"] = searchString;
         var students = from s in _context.Students
                         select s;
+        
+        if (!String.IsNullOrEmpty(searchString))
+        {
+          students = students.Where(
+            s => s.LastName.Contains(searchString) ||
+                  s.FirstMidName.Contains(searchString)
+          );
+        }
         
         switch (sortOrder)
         {
